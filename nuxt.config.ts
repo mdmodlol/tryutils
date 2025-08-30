28ea125ef2db3c8045ea3a99713b626570c60bca
mdmodlol <2149078315@qq.com>
2025-08-30 23:38:30 +0800

    feat: change project to nuxt3

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
+  compatibilityDate: '2025-08-30',
   devtools: { enabled: true },
   modules: [
     '@nuxt/eslint',
-    '@nuxt/icon'
+    'nuxt-icon',
+    '@nuxtjs/tailwindcss'
   ],
   app: {
     head: {
